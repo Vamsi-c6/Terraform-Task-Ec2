5b2ffc2d8de08d0555b211443f3939403c15e032
Pabbiti Vamsi <114283249+Vamsi-c6@users.noreply.github.com>
2025-08-21 11:00:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
      backend "s3" {
-       bucket       = "Vamsi-bucket-20082025"
+       bucket       = "vamsi-bucket-for-training200825"
        key          = "Vamsi-terraform.tfstate"
        region       = "eu-west-2"
        use_lockfile = true
